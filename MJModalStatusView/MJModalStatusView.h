da5aa5f36f0fa5ea510b34f645d62e2374a51715
Marcelo Junior <marcelo_junior07@hotmail.com>
2017-10-26 22:08:52 -0200

    Initial Commit

---
+++ MJModalStatusView/MJModalStatusView.h
@@ -0,0 +1,19 @@
+//
+//  MJModalStatusView.h
+//  MJModalStatusView
+//
+//  Created by Marcelo Junior on 26/10/17.
+//  Copyright © 2017 Marcelo Junior. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MJModalStatusView.
+FOUNDATION_EXPORT double MJModalStatusViewVersionNumber;
+
+//! Project version string for MJModalStatusView.
+FOUNDATION_EXPORT const unsigned char MJModalStatusViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MJModalStatusView/PublicHeader.h>
+
+
